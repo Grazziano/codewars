184e7ef9232e6de25892eb7b7f4b5d5e1d5301cc
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-07-22 15:19:34 -0300

    feat: Break camelCase

---
+++ js/BreakCamelCase.js
@@ -0,0 +1,18 @@
+`
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+`;
+
+function solution(string) {
+  return string.replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+    return str.toUpperCase();
+  });
+}
+
+console.log(solution("camelCasing"));
+console.log(solution("identifier"));
