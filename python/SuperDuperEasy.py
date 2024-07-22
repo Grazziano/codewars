a4aca805e16a8692ec6466dd09b6155a4d57935c
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-07-22 20:47:41 -0300

    feat: Super Duper Easy

---
+++ python/SuperDuperEasy.py
@@ -0,0 +1,13 @@
+"""
+Make a function that returns the value multiplied by 50 and increased by 6. 
+If the value entered is a string it should return "Error".
+"""
+
+def problem(a):
+    #Easy Points ^_^
+    if isinstance(a, str):
+        return "Error"
+
+    return a * 50 + 6
+
+print(problem(1))
